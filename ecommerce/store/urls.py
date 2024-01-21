a4b47266fce6feca175175aec423e3d24d8ee941
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-01-21 17:16:09 +0200

    Added files

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from ecommerce.store.views import store, cart, checkout
+
+urlpatterns = [
+    path('', store, name='store'),
+    path('cart/', cart, name='cart'),
+    path('checkout/', checkout, name='checkout'),
+]
