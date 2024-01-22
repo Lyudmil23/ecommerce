166e9b64c47187c6ee0534edf568ca05d6926624
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-01-22 10:00:51 +0200

    Added files

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(
+        max_length=200,
+        null=True,
+    )
+    email = models.CharField(
+        max_length=200,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=200,
+        null=True,
+    )
+    price = models.FloatField()
+    digital = models.BooleanField(
+        default=False,
+        null=True,
+        blank=False,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    date_ordered = models.DateTimeField(
+        auto_now_add=True,
+    )
+    complete = models.BooleanField(
+        default=False,
+        null=True,
+        blank=True,
+    )
+    transaction_id = models.CharField(
+        max_length=200,
+        null=True,
+    )
+
+    def __str__(self):
+        return str(self.id)
