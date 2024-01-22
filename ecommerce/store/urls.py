8b29b0fdb948088930f85894f4be57ac2bbe0928
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-01-22 17:33:57 +0200

    Added files

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from ecommerce.store.views import store, cart, checkout
+from ecommerce.store.views import store, cart, checkout, updateItem
 
 urlpatterns = [
     path('', store, name='store'),
     path('cart/', cart, name='cart'),
     path('checkout/', checkout, name='checkout'),
+    path('update_item/', updateItem, name='update_item'),
 ]
