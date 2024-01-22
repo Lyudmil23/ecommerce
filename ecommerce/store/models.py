d202f4fde45bfed545ea199ea2612718093cb629
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-01-22 14:12:57 +0200

    Added files

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -75,6 +75,18 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 
 class OrderItem(models.Model):
@@ -98,6 +110,11 @@ class OrderItem(models.Model):
     date_added = models.DateTimeField(
         auto_now_add=True,
     )
+    
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
