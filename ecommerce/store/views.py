b4110b7b56402e4afbf58803184d288ac4f4b4c4
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-01-23 16:25:25 +0200

    Added files

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -102,6 +102,21 @@ def processOrder(request):
         total = float(data['form']['total'])
         order.transaction_id = transaction_id
 
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+
+
     else:
         print('User is not logged in..')
     return JsonResponse('Payment complete!', safe=False)
