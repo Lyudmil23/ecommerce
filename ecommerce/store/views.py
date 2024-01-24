613bb05ef1f1834a9ae4b2a3da05e5a7a7a0689d
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-01-24 17:44:09 +0200

    Added files

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 # from django.views.decorators.csrf import csrf_exempt
 
 from .models import *
-from .utils import cookieCart, cartData
+from .utils import cookieCart, cartData, guestOrder
 
 
 def store(request):
@@ -86,24 +86,25 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
-
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                address=data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
-            )
-
 
     else:
-        print('User is not logged in..')
+        customer, order = guestOrder(request, data)
+
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
     return JsonResponse('Payment complete!', safe=False)
